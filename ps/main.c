f26cddc50a75f8bb2a7b1393b07ef914702c5479
Kei Nohguchi <kei@nohguchi.com>
2018-04-23 16:19:46 -0700

    ps: Printing all the processes!

--- ps/main.c
+++ ps/main.c
@@ -1,28 +1,18 @@
 #include <linux/types.h>
-#include <linux/list.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/printk.h>
 #include <linux/sched.h>
-#include <linux/init_task.h>
+#include <linux/sched/signal.h>
 
 static int __init ps_init(void)
 {
 	struct task_struct *task;
-	struct list_head *list;
 
-	pr_info("%s()\n", __FUNCTION__);
-
-	/* list all the children */
-	list_for_each(list, &current->children) {
-		task = list_entry(list, struct task_struct, sibling);
+	/* print all the processes */
+	for_each_process(task) {
 		printk("%s[%d]\n", task->comm, task->pid);
 	}
-
-	/* look and print the origin of all the processes */
-	for (task = current; task != &init_task; task = task->parent)
-		;
-	printk("%s[%d]\n", task->comm, task->pid);
 	return 0;
 }
 module_init(ps_init);
