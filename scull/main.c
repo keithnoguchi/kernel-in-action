64d2d25a07318e79880462904b1a16336aee80de
Kei Nohguchi <kei@nohguchi.com>
2018-04-25 10:28:20 -0700

    scull: Create a device file under /dev with device_create()

--- scull/main.c
+++ scull/main.c
@@ -11,6 +11,7 @@
 
 /* scull device driver. */
 struct scull {
+	struct device *dev;
 	struct cdev cdev;
 };
 
@@ -28,32 +29,54 @@ static struct file_operations fops = {
 
 static int __init scull_init(void)
 {
-	struct scull *dev;
+	struct scull *s;
 	int idx = 0;
 	int err;
 
 	pr_info("%s\n", __FUNCTION__);
 
-	/* allocate char device number */
+	/* allocate char device number region */
 	err = alloc_chrdev_region(&dev_number_base, idx, nr_dev, scull_dev_name);
 	if (err)
 		return err;
 	pr_info("MAJOR=%d, MINOR=%d\n", MAJOR(dev_number_base),
 		MINOR(dev_number_base));
 
+	/* scull class */
 	cls = class_create(THIS_MODULE, scull_dev_name);
-	if (IS_ERR(cls))
-		return PTR_ERR(cls);
-
-	/* initialize and add it to the char subsystem */
-	dev = &sculls[idx];
-	cdev_init(&dev->cdev, &fops);
-	dev->cdev.owner = THIS_MODULE;
-	err = cdev_add(&dev->cdev, dev_number_base, nr_dev);
-	if (err)
+	if (IS_ERR(cls)) {
+		err = PTR_ERR(cls);
 		goto unregister;
+	}
+
+	/* initialize the scull driver. */
+	s = &sculls[idx];
+	s->dev = device_create(cls, NULL, dev_number_base, s,
+			       "%s%d", scull_dev_name, idx);
+	if (IS_ERR(s->dev)) {
+		err = PTR_ERR(s->dev);
+		s->dev = NULL;
+		goto class_destroy;
+	}
+	cdev_init(&s->cdev, &fops);
+	s->cdev.owner = THIS_MODULE;
+
+	/* register the device into the character device subsystem */
+	err = cdev_device_add(&s->cdev, s->dev);
+	if (err)
+		goto device_destroy;
 
 	return 0;
+device_destroy:
+	if (!IS_ERR_OR_NULL(s->dev)) {
+		device_destroy(cls, s->dev->devt);
+		s->dev = NULL;
+	}
+class_destroy:
+	if (!IS_ERR_OR_NULL(cls)) {
+		class_destroy(cls);
+		cls = NULL;
+	}
 unregister:
 	unregister_chrdev_region(dev_number_base, nr_dev);
 	return err;
@@ -62,14 +85,14 @@ module_init(scull_init);
 
 static void __exit scull_exit(void)
 {
-	struct scull *dev;
-	int idx = 0;
+	struct scull *s;
+	int i;
 
 	pr_info("%s\n", __FUNCTION__);
-	dev = &sculls[idx];
-	cdev_del(&dev->cdev);
-	if (!IS_ERR_OR_NULL(cls))
-		class_destroy(cls);
+	for (s = sculls, i = 0; i < nr_dev; s++, i++)
+		cdev_device_del(&s->cdev, s->dev);
+
+	class_destroy(cls);
 	unregister_chrdev_region(dev_number_base, nr_dev);
 }
 module_exit(scull_exit);
