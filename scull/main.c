52cbe0ed8fedde595fc9b13f289b30b14a74aa0e
Kei Nohguchi <kei@nohguchi.com>
2018-04-25 11:29:19 -0700

    scull: Fix the scull device cleanup process in scull_init()

--- scull/main.c
+++ scull/main.c
@@ -10,6 +10,9 @@
 #include <linux/kdev_t.h>
 #include <linux/cdev.h>
 
+#define SCULL_DEV_PREFIX  "scull"
+#define NR_SCULL_DEV      4
+
 /* scull device driver. */
 struct scull {
 	struct device dev;
@@ -18,9 +21,9 @@ struct scull {
 
 /* Global variables.  Will try to clean those up. */
 static dev_t dev_number_base;
-const char *scull_dev_name = "scull";
-static const int nr_dev = 1;
-static struct scull sculls[1];
+const char *scull_dev_name = SCULL_DEV_PREFIX;
+static const int nr_dev = NR_SCULL_DEV;
+static struct scull sculls[NR_SCULL_DEV];
 
 /* File operations. */
 static struct file_operations fops = {
@@ -31,34 +34,40 @@ static int __init scull_init(void)
 {
 	struct scull *s;
 	char buf[16];
-	int idx = 0;
+	int i, j;
 	int err;
 
-	pr_info("%s\n", __FUNCTION__);
-
 	/* allocate char device number region */
-	err = alloc_chrdev_region(&dev_number_base, idx, nr_dev, scull_dev_name);
+	err = alloc_chrdev_region(&dev_number_base, 0, nr_dev, scull_dev_name);
 	if (err)
 		return err;
-	pr_info("MAJOR=%d, MINOR=%d\n", MAJOR(dev_number_base),
-		MINOR(dev_number_base));
 
-	/* initialize the scull driver. */
-	s = &sculls[idx];
-	sprintf(buf, "%s%d", scull_dev_name, idx);
-	device_initialize(&s->dev);
-	s->dev.init_name = buf;
-	s->dev.devt = MKDEV(MAJOR(dev_number_base), idx);
-	cdev_init(&s->cdev, &fops);
-	s->cdev.owner = THIS_MODULE;
+	/* create scull devices */
+	for (s = sculls, i = 0; i < nr_dev; s++, i++) {
+		device_initialize(&s->dev);
+		s->dev.devt = MKDEV(MAJOR(dev_number_base), i);
+		sprintf(buf, "%s%d", scull_dev_name, i);
+		s->dev.init_name = buf;
+		cdev_init(&s->cdev, &fops);
+		s->cdev.owner = THIS_MODULE;
 
-	/* register the device into the character device subsystem */
-	err = cdev_device_add(&s->cdev, &s->dev);
-	if (err)
-		goto unregister;
+		/* add the device into the character device subsystem */
+		err = cdev_device_add(&s->cdev, &s->dev);
+		if (err)
+			goto unregister;
+
+		pr_info("%s[%d:%d]: added\n", dev_name(&s->dev),
+			MAJOR(s->dev.devt), MINOR(s->dev.devt));
+	}
 
 	return 0;
 unregister:
+	/* only delete the added devices */
+	for (s = sculls, j = 0; j < i; s++, j++) {
+		cdev_device_del(&s->cdev, &s->dev);
+		pr_info("%s[%d:%d]: deleted\n", dev_name(&s->dev),
+			MAJOR(s->dev.devt), MINOR(s->dev.devt));
+	}
 	unregister_chrdev_region(dev_number_base, nr_dev);
 	return err;
 }
@@ -69,9 +78,11 @@ static void __exit scull_exit(void)
 	struct scull *s;
 	int i;
 
-	pr_info("%s\n", __FUNCTION__);
-	for (s = sculls, i = 0; i < nr_dev; s++, i++)
+	for (s = sculls, i = 0; i < nr_dev; s++, i++) {
 		cdev_device_del(&s->cdev, &s->dev);
+		pr_info("%s[%d:%d]: deleted\n", dev_name(&s->dev),
+			MAJOR(s->dev.devt), MINOR(s->dev.devt));
+	}
 	unregister_chrdev_region(dev_number_base, nr_dev);
 }
 module_exit(scull_exit);
