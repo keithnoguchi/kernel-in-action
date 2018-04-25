50f1fba80ebbd42c1df6b0f39136529d3cd0f81c
Kei Nohguchi <kei@nohguchi.com>
2018-04-25 08:29:42 -0700

    scull: allocate a device number region with alloc_chrdev_region()

--- scull/main.c
+++ scull/main.c
@@ -3,10 +3,24 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+
+static dev_t dev_number_base;
+const char *dev_name = "scull";
+static int nr_dev = 1;
 
 static int __init scull_init(void)
 {
+	int err;
+
 	pr_info("%s\n", __FUNCTION__);
+	err = alloc_chrdev_region(&dev_number_base, 0, nr_dev, dev_name);
+	if (err)
+		return err;
+	pr_info("MAJOR=%d, MINOR=%d\n", MAJOR(dev_number_base),
+		MINOR(dev_number_base));
+
 	return 0;
 }
 module_init(scull_init);
@@ -14,6 +28,7 @@ module_init(scull_init);
 static void __exit scull_exit(void)
 {
 	pr_info("%s\n", __FUNCTION__);
+	unregister_chrdev_region(dev_number_base, nr_dev);
 }
 module_exit(scull_exit);
 
