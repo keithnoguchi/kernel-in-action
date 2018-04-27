4be9294ab0b6f10ca676da3a900316cd7d0d578a
Kei Nohguchi <kei@nohguchi.com>
2018-04-26 17:21:48 -0700

    scull/tests: Added qset ioctl() tests

--- scull/tests/main.c
+++ scull/tests/main.c
@@ -10,6 +10,92 @@
 
 #include "../scull.h"
 
+static int test_scull_ioctl_qset(const char *name, const char *scull)
+{
+	const char *cmd;
+	int set_qset, get_qset;
+	int err;
+	int fd;
+
+	printf("%s\n", name);
+
+	/* open scull device. */
+	fd = open(scull, O_RDONLY);
+	if (fd == -1) {
+		perror("open");
+		return EXIT_FAILURE;
+	}
+
+	/* reset the value first before the test */
+	err = ioctl(fd, SCULL_IOCRESET);
+	if (err == -1) {
+		cmd = "ioctl(SCULL_IOCRESET)";
+		goto out;
+	}
+
+	/* set and get qset */
+	set_qset = 10;
+	err = ioctl(fd, SCULL_IOCSQSET, &set_qset);
+	if (err == -1) {
+		cmd = "ioctl(SCULL_IOCSQSET)";
+		goto out;
+	}
+	err = ioctl(fd, SCULL_IOCGQSET, &get_qset);
+	if (err == -1) {
+		cmd = "ioctl(SCULL_IOCGQSET)";
+		goto out;
+	}
+	assert(get_qset == set_qset);
+
+	/* tell and query qset */
+	set_qset = 11;
+	err = ioctl(fd, SCULL_IOCTQSET, set_qset);
+	if (err == -1) {
+		cmd = "ioctl(SCULL_IOCTQSET)";
+		goto out;
+	}
+	get_qset = ioctl(fd, SCULL_IOCQQSET);
+	if (get_qset == -1) {
+		err = get_qset;
+		cmd = "ioctl(SCULL_IOCQQSET)";
+		goto out;
+	}
+	assert(get_qset == set_qset);
+
+	/* exchange qset */
+	set_qset = 1000;
+	get_qset = set_qset;
+	err = ioctl(fd, SCULL_IOCXQSET, &get_qset);
+	if (err == -1) {
+		cmd = "ioctl(SCULL_IOCXQSET)";
+		goto out;
+	}
+	assert(get_qset == set_qset);
+
+	/* shift qset */
+	set_qset = 2;
+	get_qset = ioctl(fd, SCULL_IOCHQSET, set_qset);
+	if (get_qset == -1) {
+		err = get_qset;
+		cmd = "ioctl(SCULL_IOCHQSET)";
+		goto out;
+	}
+	assert(get_qset == set_qset);
+out:
+	/* reset and close the device before exiting */
+	if (ioctl(fd, SCULL_IOCRESET))
+		perror("ioctl(SCULL_IOCRESET)");
+	if (close(fd))
+		perror("close()");
+
+	/* report ioctl() command, in case of error */
+	if (err < 0) {
+		perror(cmd);
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
 static int test_scull_ioctl_quantum(const char *name, const char *scull)
 {
 	const char *cmd;
@@ -98,5 +184,15 @@ out:
 
 int main(void)
 {
-	test_scull_ioctl_quantum("1. ioctl(SCULL_IOC?QUANTUM)", "/dev/scull0");
+	int err;
+
+	err = test_scull_ioctl_qset("1. ioctl(SCULL_IOC?QSET)", "/dev/scull1");
+	if (err)
+		return err;
+
+	err = test_scull_ioctl_quantum("2. ioctl(SCULL_IOC?QUANTUM)", "/dev/scull2");
+	if (err)
+		return err;
+
+	return err;
 }
