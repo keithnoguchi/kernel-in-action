bc93e754755e8fcdd3cda2d41410a4d9019c80cb
Kei Nohguchi <kei@nohguchi.com>
2018-04-30 12:07:06 -0700

    scullp: Add select() based timer behavior

--- scullp/tests/select.c
+++ scullp/tests/select.c
@@ -3,8 +3,26 @@
 #include <stdio.h>
 #include <sys/select.h>
 
+#define SELECT_INTERVAL		2	/* wakes up every 2 sec */
+
 int main(void)
 {
-	printf("select(2) based tests\n");
-	return 0;
+	struct timeval tv;
+	fd_set rfds;
+	int ret;
+
+	printf("select(2) based scullp.ko tests\n");
+
+	while (1) {
+		tv.tv_sec = SELECT_INTERVAL;
+		tv.tv_usec = 0;
+
+		ret = select(1, &rfds, NULL, NULL, &tv);
+		if (ret == -1) {
+			perror("select");
+			break;
+		}
+		printf("wakeup\n");
+	}
+	return ret;
 }
