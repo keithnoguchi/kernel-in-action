e280041164dfcc3c8a1687310f12b5e03d1fb01e
Kei Nohguchi <kei@nohguchi.com>
2018-05-02 08:53:51 -0700

    sculld: Add ldd_driver base structure
    This will be the base structure for all the ldd bus drivers.

--- sculld/main.c
+++ sculld/main.c
@@ -4,6 +4,16 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/device.h>
+#include <linux/string.h>
+
+/* ldd driver */
+struct ldd_driver {
+	const char		*version;
+	struct module		*module;
+	struct device_driver	driver;
+	struct driver_attribute	version_attr;
+};
+#define to_ldd_driver(_drv)	container_of(_drv, struct ldd_driver, driver)
 
 static void ldd_bus_release(struct device *d)
 {
@@ -32,6 +42,43 @@ static struct bus_type ldd_bus_type = {
 	.match = ldd_match,
 	.uevent = ldd_uevent,
 };
+
+static ssize_t show_version(struct device_driver *drv, char *buf)
+{
+	const struct ldd_driver *ldrv = to_ldd_driver(drv);
+
+	sprintf(buf, "%s\n", ldrv->version);
+	return strlen(buf);
+}
+
+int register_ldd_driver(struct ldd_driver *drv)
+{
+	int err;
+
+	drv->driver.bus = &ldd_bus_type;
+	err = driver_register(&drv->driver);
+	if (err)
+		return err;
+	drv->version_attr.attr.name = "version";
+	drv->version_attr.attr.mode = S_IRUGO;
+	drv->version_attr.show = show_version;
+	drv->version_attr.store = NULL;
+	err = driver_create_file(&drv->driver, &drv->version_attr);
+	if (err)
+		goto unregister;
+	return 0;
+unregister:
+	driver_unregister(&drv->driver);
+	return err;
+}
+EXPORT_SYMBOL(register_ldd_driver);
+
+void unregister_ldd_driver(struct ldd_driver *drv)
+{
+	driver_remove_file(&drv->driver, &drv->version_attr);
+	driver_unregister(&drv->driver);
+}
+EXPORT_SYMBOL(unregister_ldd_driver);
 
 static int __init sculld_init(void)
 {
