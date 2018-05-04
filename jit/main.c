a0764a8ee148f039e2eaa43860bfa1dff9ee0955
Kei Nohguchi <kei@nohguchi.com>
2018-05-04 15:20:37 -0700

    jit: Increment the JIT currenttime time index through seq_file

--- jit/main.c
+++ jit/main.c
@@ -3,28 +3,98 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/jiffies.h>
+#include <linux/err.h>
+#include <linux/fs.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#include <linux/err.h>
 
-static struct file_operations jit_currenttime_ops = {
+enum jit_procfs_ct_type {
+	JIT_PROCFS_CT_NONE = 0,
+	JIT_PROCFS_CT_JIFFIES,
+	JIT_PROCFS_CT_JIFFIES64,
+	JIT_PROCFS_CT_DO_GETTIMEOFDAY,
+	JIT_PROCFS_CT_CURRENT_KERNEL_TIME,
+	JIT_PROCFS_CT_MAX,
+};
+
+static void *jit_procfs_ct_seq_start(struct seq_file *s, loff_t *pos)
+{
+	pr_info("%s\n", __FUNCTION__);
+	*pos = JIT_PROCFS_CT_JIFFIES;
+	return (void *)JIT_PROCFS_CT_JIFFIES;
+}
+
+static void *jit_procfs_ct_seq_next(struct seq_file *s, void *v, loff_t *pos)
+{
+	enum jit_procfs_ct_type type = (enum jit_procfs_ct_type)v;
+	if ((*pos) == JIT_PROCFS_CT_MAX)
+		return NULL;
+	type++; (*pos)++;
+	pr_info("%s(%d)\n", __FUNCTION__, type);
+	return (void *)type;
+}
+
+static void jit_procfs_ct_seq_stop(struct seq_file *s, void *v)
+{
+	enum jit_procfs_ct_type type = (enum jit_procfs_ct_type)v;
+	pr_info("%s(%d)\n", __FUNCTION__, type);
+	return;
+}
+
+static int jit_procfs_ct_seq_show(struct seq_file *s, void *v)
+{
+	enum jit_procfs_ct_type type = (enum jit_procfs_ct_type)v;
+	pr_info("%s(%d)\n", __FUNCTION__, type);
+	seq_printf(s, "JIT currenttime type: %d\n", type);
+	return 0;
+}
+
+const static struct seq_operations jit_procfs_ct_seq_ops = {
+	.start = jit_procfs_ct_seq_start,
+	.next = jit_procfs_ct_seq_next,
+	.stop = jit_procfs_ct_seq_stop,
+	.show = jit_procfs_ct_seq_show,
+};
+
+static int jit_procfs_ct_open(struct inode *i, struct file *f)
+{
+	pr_info("%s\n", __FUNCTION__);
+	return seq_open(f, &jit_procfs_ct_seq_ops);
+}
+
+const static struct file_operations jit_procfs_ct_ops = {
 	.owner = THIS_MODULE,
-	//.open = jit_currenttime_open,
+	.open = jit_procfs_ct_open,
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = seq_release,
 };
 
-static int __init jit_init(void)
+static int jit_init_procfs(void)
 {
 	struct proc_dir_entry *entry;
 
-	pr_info("%s\n", __FUNCTION__);
-
-	entry = proc_create("currenttime", S_IRUGO, NULL, &jit_currenttime_ops);
+	entry = proc_create("currenttime", S_IRUGO, NULL, &jit_procfs_ct_ops);
 	if (IS_ERR(entry))
 		return PTR_ERR(entry);
+
+	return 0;
+}
+
+static void jit_exit_procfs(void)
+{
+	remove_proc_entry("currenttime", NULL);
+}
+
+static int __init jit_init(void)
+{
+	int err;
+
+	pr_info("%s\n", __FUNCTION__);
+
+	err = jit_init_procfs();
+	if (err)
+		return err;
 
 	return 0;
 }
@@ -33,7 +103,7 @@ module_init(jit_init);
 static void __exit jit_exit(void)
 {
 	pr_info("%s\n", __FUNCTION__);
-	remove_proc_entry("currenttime", NULL);
+	jit_exit_procfs();
 }
 module_exit(jit_exit);
 
--- jit/tests/jit_procfs_test.c
+++ jit/tests/jit_procfs_test.c
@@ -1,13 +1,51 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "kselftest.h"
 
 static int procfs_test(int *i)
 {
-	ksft_inc_fail_cnt();
-	return 1;
+	const struct test {
+		const char	*name;
+		const char	*file_name;
+		int		flags;
+	} tests[] = {
+		{
+			.name = "/proc/currenttime file",
+			.file_name = "/proc/currenttime",
+			.flags = O_RDONLY,
+		},
+		{ /* sentory */ },
+	};
+	const struct test *t;
+
+	for (t = tests; t->name; t++) {
+		printf("%2d) %-70s", (*i)++, t->name);
+		int fd;
+
+		fd = open(t->file_name, t->flags);
+		if (fd == -1) {
+			perror("open");
+			goto fail;
+		}
+		if (close(fd)) {
+			perror("close");
+			fd = -1;
+			goto fail;
+		}
+		ksft_inc_pass_cnt();
+		continue;
+fail:
+		ksft_inc_fail_cnt();
+		if (fd != -1)
+			close(fd);
+	}
+	return 0;
 }
 
 int main(void)
