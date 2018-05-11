26d8ac64b6dd40f61a7103a37b5bccb3421c9170
Kei Nohguchi <kei@nohguchi.com>
2018-05-10 18:04:03 -0700

    snull: Allocate and free the netdevice through the module load/unload

--- snull/main.c
+++ snull/main.c
@@ -3,19 +3,54 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/netdevice.h>
 
-static int __init snull_init(void)
+/* sn0 and sn1 */
+static struct net_device *netdevs[2];
+
+/* snull driver */
+struct snull { };
+
+static void snull_init(struct net_device *dev)
 {
+	pr_info("%s(%s)\n", __FUNCTION__, netdev_name(dev));
+}
+
+static int __init snull_init_module(void)
+{
+	int i;
+
 	pr_info("%s\n", __FUNCTION__);
+
+	for (i = 0; i < ARRAY_SIZE(netdevs); i++) {
+		struct net_device *dev;
+
+		dev = alloc_netdev(sizeof(struct snull), "sn%d",
+				   NET_NAME_UNKNOWN, snull_init);
+		if (!dev)
+			goto free_netdev;
+		netdevs[i] = dev;
+	}
 	return 0;
+free_netdev:
+	for (i = 0; i < ARRAY_SIZE(netdevs); i++) {
+		if (netdevs[i])
+			free_netdev(netdevs[i]);
+		netdevs[i] = NULL;
+	}
+	return -ENOMEM;
 }
-module_init(snull_init);
+module_init(snull_init_module);
 
-static void __exit snull_exit(void)
+static void __exit snull_cleanup_module(void)
 {
+	int i;
+
 	pr_info("%s\n", __FUNCTION__);
+	for (i = 0; i < ARRAY_SIZE(netdevs); i++)
+		free_netdev(netdevs[i]);
 }
-module_exit(snull_exit);
+module_exit(snull_cleanup_module);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kei Nohguchi <kei@nohguchi.com>");
