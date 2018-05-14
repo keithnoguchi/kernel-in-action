b74a6b66ad740b0e375a402b532bd60509197194
Kei Nohguchi <kei@nohguchi.com>
2018-05-14 09:27:23 -0700

    snull: Add test to check the IFF_NOARP flags

--- snull/tests/snull_sysfs_test.c
+++ snull/tests/snull_sysfs_test.c
@@ -1,10 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <linux/if.h>
 
 #include "kselftest.h"
 
@@ -33,6 +36,16 @@ static int sysfs_open_test(int *i)
 		{
 			.name = "/sys/devices/virtual/net/sn1/mtu file",
 			.file_name = "/sys/devices/virtual/net/sn1/mtu",
+			.flags = O_RDWR,
+		},
+		{
+			.name = "/sys/devices/virtual/net/sn0/flags file",
+			.file_name = "/sys/devices/virtual/net/sn0/flags",
+			.flags = O_RDWR,
+		},
+		{
+			.name = "/sys/devices/virtual/net/sn1/flags file",
+			.file_name = "/sys/devices/virtual/net/sn1/flags",
 			.flags = O_RDWR,
 		},
 		{ /* sentry */ },
@@ -66,12 +79,82 @@ fail:
 	return fail;
 }
 
+static int sysfs_flags_test(int *i)
+{
+	const struct test {
+		const char	*name;
+		const char	*dev_name;
+		long		flags;
+		long		want;
+	} tests[] = {
+		{
+			.name		= "sn0 IFF_NOARP check",
+			.dev_name	= "sn0",
+			.flags		= IFF_NOARP,
+			.want		= IFF_NOARP,
+		},
+		{
+			.name		= "sn1 IFF_NOARP check",
+			.dev_name	= "sn1",
+			.flags		= IFF_NOARP,
+			.want		= IFF_NOARP,
+		},
+		{ /* sentry */ },
+	};
+	const char *path = "/sys/devices/virtual/net";
+	const struct test *t;
+	int fail = 0;
+
+	for (t = tests; t->name; t++) {
+		char file[BUFSIZ];
+		char buf[BUFSIZ];
+		int fd = -1;
+		long flags;
+		int err;
+
+		printf("%3d) %-70s", ++(*i), t->name);
+
+		err = snprintf(file, BUFSIZ, "%s/%s/flags", path, t->dev_name);
+		if (err == -1)
+			goto fail;
+		fd = open(file, O_RDWR);
+		if (fd == -1)
+			goto fail;
+		err = read(fd, buf, BUFSIZ);
+		if (err <= 0)
+			goto fail;
+		buf[err] = '\0';
+		flags = strtol(buf, NULL, 0);
+		if (flags == -1)
+			goto fail;
+		if ((flags & t->flags) != t->want)
+			goto fail;
+		if (close(fd)) {
+			fd = -1;
+			goto fail;
+		}
+		ksft_inc_pass_cnt();
+		puts("PASS");
+		continue;
+fail:
+		ksft_inc_fail_cnt();
+		puts("FAIL");
+		fail++;
+		if (fd != -1)
+			close(fd);
+		fd = -1;
+	}
+	return fail;
+}
+
 int main(void)
 {
 	int fail = 0;
 	int i = 0;
 
 	if (sysfs_open_test(&i))
+		fail++;
+	if (sysfs_flags_test(&i))
 		fail++;
 
 	puts("");
