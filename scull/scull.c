0604727d7c28fa712c7aba02e778e6296067dd6f
Kei Nohguchi <kei@nohguchi.com>
2018-11-14 10:24:10 -0800

    scull: Allocate/release the char dev region for scull

--- scull/scull.c
+++ scull/scull.c
@@ -2,17 +2,36 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+
+/* scull globals */
+static struct scull {
+	dev_t		dev_base;
+	int		dev_count;
+	const char	*dev_name;
+} scull = {
+	.dev_name	= "scull",
+	.dev_count	= 1,
+};
 
 static int scull_init(void)
 {
-	printk("scull_init()\n");
-	return 0;
+	int err;
+
+	err = alloc_chrdev_region(&scull.dev_base, 0, scull.dev_count, scull.dev_name);
+	if (err < 0)
+		goto out;
+out:
+	printk("scull_init(%d:%d)\n", MAJOR(scull.dev_base), MINOR(scull.dev_base));
+	return err;
 }
 module_init(scull_init);
 
 static void scull_exit(void)
 {
-	printk("scull_exit()\n");
+	printk("scull_exit(%d:%d)\n", MAJOR(scull.dev_base), MINOR(scull.dev_base));
+	unregister_chrdev_region(scull.dev_base, scull.dev_count);
 }
 module_exit(scull_exit);
 
